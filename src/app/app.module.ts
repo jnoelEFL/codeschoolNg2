1603d7ea32b61e9cec992ab1960ea8373a17e54a
Julien Noel <j.noel@lefebvre-sarrut.eu>
2017-09-26 14:26:55 +0200

    Added detail route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RacesComponent } from './races/races.component';
 import { RaceService } from './races/race.service';
+import { RaceComponent } from './race/race.component';
+import { RaceDetailComponent } from './race-detail/race-detail.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'races', pathMatch: 'full'},
+  { path: 'races', component: RacesComponent },
+  { path: 'race/:id', component: RaceDetailComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    RacesComponent
+    RacesComponent,
+    RaceComponent,
+    RaceDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [ RaceService ],
   bootstrap: [AppComponent]
